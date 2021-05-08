3122c7d00b97d420eb6c89232de835c7c5790c6d
JR <hermenes@protonmail.com>
2021-05-08 15:25:20 +0100

    Add more tests

--- tests/integration/municipalities.test.ts
+++ tests/integration/municipalities.test.ts
@@ -1,6 +1,7 @@
 import { agent } from 'supertest'
 import { app } from '../../src/app'
 import { basePath } from '../helpers'
+import { munipalitiesTestData } from '../test-data/Municipalities'
 
 const appWithoutRunLogs = agent(app)
 
@@ -11,6 +12,21 @@ describe('GET /municipalities', () => {
 			.expect(200)
 			.then(({ body }) => {
 				expect(body).toHaveLength(309)
+				done()
+			})
+	})
+	it('should return municipalities in correct order', async done => {
+		await appWithoutRunLogs
+			.get(`${basePath}/municipalities`)
+			.expect(200)
+			.then(({ body }) => {
+				const alajarviMunicipality = body[0]
+				expect(alajarviMunicipality).toHaveProperty('official_id')
+				expect(alajarviMunicipality).toHaveProperty('name')
+				expect(alajarviMunicipality).toHaveProperty('file_url')
+				expect(body[0]).toStrictEqual(munipalitiesTestData.municipality1)
+				expect(body[1]).toStrictEqual(munipalitiesTestData.municipality2)
+				expect(body[308]).toStrictEqual(munipalitiesTestData.municipality3)
 				done()
 			})
 	})
