38bae940f0fdb639922c9ea2ce7f5996eb8999f8
jaraisanen <hermenes@protonmail.com>
2021-07-21 16:17:57 +0100

    Update deps

--- tests/integration/municipalities.test.ts
+++ tests/integration/municipalities.test.ts
@@ -6,16 +6,15 @@ import { munipalitiesTestData } from '../test-data/Municipalities'
 const server = agent(app)
 
 describe('GET /municipalities', () => {
-	it('should return all municipalities', async done => {
+	it('should return all municipalities', async () => {
 		await server
 			.get(`${basePath}/municipalities`)
 			.expect(200)
 			.then(({ body }) => {
 				expect(body).toHaveLength(309)
-				done()
 			})
 	})
-	it('should return municipalities in correct order', async done => {
+	it('should return municipalities in correct order', async () => {
 		await server
 			.get(`${basePath}/municipalities`)
 			.expect(200)
@@ -27,7 +26,6 @@ describe('GET /municipalities', () => {
 				expect(body[0]).toStrictEqual(munipalitiesTestData.municipality1)
 				expect(body[1]).toStrictEqual(munipalitiesTestData.municipality2)
 				expect(body[308]).toStrictEqual(munipalitiesTestData.municipality3)
-				done()
 			})
 	})
 })
--- tests/integration/municipality.test.ts
+++ tests/integration/municipality.test.ts
@@ -6,7 +6,7 @@ import { munipalitiesTestData } from '../test-data/Municipalities'
 const server = agent(app)
 
 describe('GET /municipality', () => {
-	it('should return existing municipality wit correct id', async done => {
+	it('should return existing municipality wit correct id', async () => {
 		await server
 			.get(`${basePath}/municipality/005`)
 			.expect(200)
@@ -17,17 +17,15 @@ describe('GET /municipality', () => {
 				expect(municipality).toHaveProperty('name')
 				expect(municipality).toHaveProperty('file_url')
 				expect(body[0]).toStrictEqual(munipalitiesTestData.municipality1)
-				done()
 			})
 	})
-	it('should return 404 with non-existing id', async done => {
+	it('should return 404 with non-existing id', async () => {
 		await server
 			.get(`${basePath}/municipality/20263164`)
 			.expect(404)
 			.then(({ body }) => {
 				expect(body).toHaveProperty('error')
 				expect(body.error).toBe('No municipality found with id: 20263164')
-				done()
 			})
 	})
 })
